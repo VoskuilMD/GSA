dcaf095acc709f32ba07d39e79abe1f734ad1f33
VoskuilMD <33254072+VoskuilMD@users.noreply.github.com>
2017-11-10 14:32:24 +0100

    Update create_HRC_jobs.sh

--- Tools/create_HRC_jobs.sh
+++ Tools/create_HRC_jobs.sh
@@ -10,5 +10,6 @@ echo ". set_HRC_variables.sh" >> HRC-1000G-check-bim."$i".sh
 echo "cd" '$RUNDIR/imputation/'""$i"" >> HRC-1000G-check-bim."$i".sh
 echo "module load R" >> HRC-1000G-check-bim."$i".sh
 echo "module load Perl" >> HRC-1000G-check-bim."$i".sh
+echo "module load Java" >> HRC-1000G-check-bim."$i".sh
 echo "perl" '$RUNDIR/scripts/HRC-1000G-check-bim.pl' "-b" '$RUNDIR/imputation/'""$i"/GSA-"$i".bim -f" '$RUNDIR/imputation/'""$i"/GSA-"$i".frq -r" '$RUNDIR/imputation/HRC.r1-1.GRCh37.wgs.mac5.sites.tab'" -h" >> HRC-1000G-check-bim."$i".sh;
 done
