4151c0e4ca9b16212add31e2d5fc1b650a304d17
VoskuilMD <33254072+VoskuilMD@users.noreply.github.com>
2017-11-10 15:23:06 +0100

    Update create_opticall_jobs.sh

--- Tools/create_opticall_jobs.sh
+++ Tools/create_opticall_jobs.sh
@@ -24,6 +24,7 @@ echo "#SBATCH --time=24:00:00" >> chr_"$i"_opticall.sh
 echo "#SBATCH --nodes 1" >> chr_"$i"_opticall.sh
 #echo "#SBATCH --open-mode=append" >> chr_"$i"_opticall.sh
 #echo "#SBATCH --export=NONE" >> chr_"$i"_opticall.sh
+echo ". set_opticall_variables.sh" >> chr_"$i"_opticall.sh
 echo "module load opticall" >> chr_"$i"_opticall.sh
 echo "cd" $RUNDIR/opticall_input >> chr_"$i"_opticall.sh
 echo '$EBROOTOPTICALL/opticall' "-in chr_"$i" -info" $info "-out ../opticall_output/chr_"$i"" -"$i" >> chr_"$i"_opticall.sh 
