740195adbf12a0794b3c7fc4bd1ce53768d36e73
VoskuilMD <33254072+VoskuilMD@users.noreply.github.com>
2017-11-10 15:22:50 +0100

    Update create_opticall_jobs.sh

--- Tools/create_opticall_jobs.sh
+++ Tools/create_opticall_jobs.sh
@@ -1,29 +1,30 @@
 # Create opticall jobs for chromosomes 1-22
 for i in {1..22}
 do
-echo "#!/bin/bash" >> scripts/chr_"$i"_opticall.sh
-echo "#SBATCH --job-name=chr_"$i"_opticall" >> scripts/chr_"$i"_opticall.sh
-echo "#SBATCH --mem 10gb" >> scripts/chr_"$i"_opticall.sh
-echo "#SBATCH --time=24:00:00" >> scripts/chr_"$i"_opticall.sh
-echo "#SBATCH --nodes 1" >> scripts/chr_"$i"_opticall.sh
-#echo "#SBATCH --open-mode=append" >> scripts/chr_"$i"_opticall.sh
-#echo "#SBATCH --export=NONE" >> scripts/chr_"$i"_opticall.sh
-echo "module load opticall" >> scripts/chr_"$i"_opticall.sh
-echo "cd" $RUNDIR/opticall_input >> scripts/chr_"$i"_opticall.sh
-echo '$EBROOTOPTICALL/opticall' "-in chr_"$i" -info" $info "-out ../opticall_output/chr_"$i"" >> scripts/chr_"$i"_opticall.sh 
+echo "#!/bin/bash" >> chr_"$i"_opticall.sh
+echo "#SBATCH --job-name=chr_"$i"_opticall" >> chr_"$i"_opticall.sh
+echo "#SBATCH --mem 10gb" >> chr_"$i"_opticall.sh
+echo "#SBATCH --time=24:00:00" >> chr_"$i"_opticall.sh
+echo "#SBATCH --nodes 1" >> chr_"$i"_opticall.sh
+#echo "#SBATCH --open-mode=append" >> chr_"$i"_opticall.sh
+#echo "#SBATCH --export=NONE" >> chr_"$i"_opticall.sh
+echo ". set_opticall_variables.sh" >> chr_"$i"_opticall.sh
+echo "module load opticall" >> chr_"$i"_opticall.sh
+echo "cd" $RUNDIR/opticall_input >> chr_"$i"_opticall.sh
+echo '$EBROOTOPTICALL/opticall' "-in chr_"$i" -info" $info "-out ../opticall_output/chr_"$i"" >> chr_"$i"_opticall.sh 
 done
 
 # Create opticall jobs for chromosomes X,Y,MT
 for i in {X,Y,MT}
 do
-echo "#!/bin/bash" >> scripts/chr_"$i"_opticall.sh
-echo "#SBATCH --job-name=chr_"$i"_opticall" >> scripts/chr_"$i"_opticall.sh
-echo "#SBATCH --mem 10gb" >> scripts/chr_"$i"_opticall.sh
-echo "#SBATCH --time=24:00:00" >> scripts/chr_"$i"_opticall.sh
-echo "#SBATCH --nodes 1" >> scripts/chr_"$i"_opticall.sh
-#echo "#SBATCH --open-mode=append" >> scripts/chr_"$i"_opticall.sh
-#echo "#SBATCH --export=NONE" >> scripts/chr_"$i"_opticall.sh
-echo "module load opticall" >> scripts/chr_"$i"_opticall.sh
-echo "cd" $RUNDIR/opticall_input >> scripts/chr_"$i"_opticall.sh
-echo '$EBROOTOPTICALL/opticall' "-in chr_"$i" -info" $info "-out ../opticall_output/chr_"$i"" -"$i" >> scripts/chr_"$i"_opticall.sh 
+echo "#!/bin/bash" >> chr_"$i"_opticall.sh
+echo "#SBATCH --job-name=chr_"$i"_opticall" >> chr_"$i"_opticall.sh
+echo "#SBATCH --mem 10gb" >> chr_"$i"_opticall.sh
+echo "#SBATCH --time=24:00:00" >> chr_"$i"_opticall.sh
+echo "#SBATCH --nodes 1" >> chr_"$i"_opticall.sh
+#echo "#SBATCH --open-mode=append" >> chr_"$i"_opticall.sh
+#echo "#SBATCH --export=NONE" >> chr_"$i"_opticall.sh
+echo "module load opticall" >> chr_"$i"_opticall.sh
+echo "cd" $RUNDIR/opticall_input >> chr_"$i"_opticall.sh
+echo '$EBROOTOPTICALL/opticall' "-in chr_"$i" -info" $info "-out ../opticall_output/chr_"$i"" -"$i" >> chr_"$i"_opticall.sh 
 done
